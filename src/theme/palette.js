d8c839d37f0f7aad46b6d43be03206f41a5bc406
Juan Jose Rendon <juan.rendon@yuxiglobal.com>
2020-06-13 17:30:45 -0500

    Feat: Create theme and add pallet to version control

---
+++ src/theme/palette.js
@@ -0,0 +1,26 @@
+const white = "#FFF"
+const black = "#000"
+
+const palette = {
+  common: {
+    black,
+    white,
+  },
+  primary: "rgba(31, 187, 180, 1)",
+  secondary: "rgba(255, 173, 27, 1)",
+  grey: {
+    "50": white,
+    "100": "#EBEBEB",
+    "200": "#DBDAD8",
+    "300": "#CCCCCB",
+    "400": "#AAAAAB",
+    "500": "#999999",
+    "600": "#666666",
+  },
+  transparent: {
+    main: "rgba(235,235,235,0.7)",
+    bg: "rgba(0,0,0,0.3)",
+  },
+}
+
+export default palette
