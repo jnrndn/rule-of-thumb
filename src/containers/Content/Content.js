947ebf955c2761fccc479d22d8dae9cffe6a6453
Juan Jose Rendon <juan.rendon@yuxiglobal.com>
2020-06-15 18:32:57 -0500

    Fix: orchestrate layout in a better way

---
+++ src/containers/Content/Content.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Home from "../../components/Home/Home"
+import Header from "../Header/Header"
+import Banner from "./../Banner/Banner"
+
+const Content = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Home />
+    </div>
+  )
+}
+
+export default Content
